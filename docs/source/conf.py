82fdb8964436a40123119c6715fc0683543be925
cmaceves <christineaceves22@gmail.com>
2023-01-23 13:04:43 -0800

    updating the path to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0,'/Users/sarahrandall/Python-outbreak-info/outbreak_data')
+sys.path.insert(0, os.path.abspath('./outbreak_data/'))
 
 
 # -- Project information -----------------------------------------------------
